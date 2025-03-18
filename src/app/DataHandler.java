d8c334dbf17ca5b490dc64b019b77dd53cb50950
VladislavSCH <114906748+VladislavSCH@users.noreply.github.com>
2025-03-18 14:41:17 +0200

    Updata app LockerApp

--- src/app/DataHandler.java
+++ src/app/DataHandler.java
@@ -6,7 +6,7 @@ import java.util.concurrent.locks.ReentrantLock;
 public class DataHandler {
     private final Lock lock = new ReentrantLock();
 
-    public synchronized int modify(int num){
+    public int modify(int num){
         lock.lock();
         try{
             num = num * 3;
